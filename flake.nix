f5d6a1b248b844cb4966b74fc6d58212933cf0b5
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-08 00:08:40 +0100

    Added HM and AAGL inputs

--- flake.nix
+++ flake.nix
@@ -2,12 +2,24 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+        home-manager = {
+            url = "github:nix-community/home-manager/release-23.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        aagl-on-nix = {
+            url = "github:ezKEa/aagl-gtk-on-nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
     outputs = {
         self,
         nixpkgs,
-        nixpkgs-unstable
+        nixpkgs-unstable,
+        home-manager,
+        aagl-on-nix
     }: {
     };
 }
