61ac84cd97f99f495dfdca50a1e4dde587b1c85b
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 11:14:18 +0100

    Added locale config

--- nixos/maxime/locale.nix
+++ nixos/maxime/locale.nix
@@ -1,4 +1,23 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    specialArgs,
+    unstable
 }: {
+    time.timeZone = "Europe/Paris";
+
+    i18n = {
+        supportedLocales = [
+            "en_US.UTF-8/UTF-8"
+            "fr_FR.UTF-8/UTF-8"
+            "ja_JP.UTF-8/UTF-8"
+        ];
+        defaultLocale = "en_US.UTF-8";
+    };
+
+    console.keyMap = "fr";
+    services.xserver.xkb.layout = "fr";
 }
