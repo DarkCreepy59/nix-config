b393f8b37aaac75cda57a0aa625fda1685729155
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 10:59:39 +0100

    Added user

--- nixos/maxime/users.nix
+++ nixos/maxime/users.nix
@@ -1,4 +1,26 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    pkgs,
+    specialArgs,
+    unstable
 }: {
+    programs.zsh.enable = true;
+
+    users.users = {
+        "max" = {
+            isNormalUser = true;
+            extraGroups = [
+                "adbusers"
+                "realtime"
+                "wheel"
+            ];
+            shell = pkgs.zsh;
+        };
+    };
+
+    security.rtkit.enable = true;
 }
