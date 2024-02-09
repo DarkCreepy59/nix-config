8cec5be9a5b9a6f33d3698e1d79f9a29f7eb98e5
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 11:20:52 +0100

    Added fonts

--- nixos/maxime/fonts.nix
+++ nixos/maxime/fonts.nix
@@ -1,4 +1,19 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    pkgs,
+    specialArgs,
+    unstable
 }: {
+    fonts.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    ];
 }
