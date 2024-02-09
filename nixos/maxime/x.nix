5394897187d1fb2554de4f52e40f175f97b429e0
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 11:25:44 +0100

    Added X config

--- nixos/maxime/x.nix
+++ nixos/maxime/x.nix
@@ -1,4 +1,43 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    pkgs,
+    specialArgs,
+    unstable
 }: {
+    services = {
+        xserver = {
+            enable = true;
+
+            libinput = {
+                touchpad = {
+                    tapping = true;
+                    naturalScrolling = true;
+                };
+            };
+
+            videoDrivers = [ "nvidia" ];
+
+            displayManager = {
+                sddm = {
+                    enable = true;
+                    autoNumlock = true;
+                    theme = "sddm-theme-corners";
+                };
+            };
+
+            desktopManager.session = [
+                {
+                    name = "xsession";
+                    start = ''
+                        ${pkgs.runtimeShell} $HOME/.xsession&
+                        waitPID=$!
+                    '';
+                }
+            ];
+        };
+    };
 }
