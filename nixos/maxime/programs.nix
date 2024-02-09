9192391d9d6de8c68ba8bd1ec7f5ae1f427a03a1
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 11:10:43 +0100

    Added programs

--- nixos/maxime/programs.nix
+++ nixos/maxime/programs.nix
@@ -1,4 +1,30 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    pkgs,
+    specialArgs,
+    unstable
 }: {
+    programs = {
+        git.enable = true;
+        neovim.enable = true;
+        neovim.package = unstable.neovim-unwrapped;
+        nano.enable = false; # Because we use neovim
+        adb.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+        acpi
+        brightnessctl
+        btop
+        file
+        neofetch
+        parallel
+        unzip
+        wget
+        zip
+    ];
 }
