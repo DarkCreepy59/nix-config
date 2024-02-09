f7fb5ac59be183aaf5ace7ed65a9605f0f6ffd1e
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 10:51:00 +0100

    Added boot config

--- nixos/maxime/boot.nix
+++ nixos/maxime/boot.nix
@@ -1,4 +1,32 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    specialArgs,
+    unstable
 }: {
+    boot = {
+        loader = {
+            systemd-boot = {
+                enable = true;
+                editor = false;
+            };
+            efi.canTouchEfiVariables = true;
+            timeout = 0;
+        };
+
+        initrd.systemd = {
+            enable = true;
+            emergencyAccess = false;
+            network.wait-online.enable = false;
+        };
+
+        kernelPackages = unstable.linuxKernel.packages.linux_zen;
+
+        tmp.useTmpfs = true;
+    };
+
+    systemd.package = unstable.systemd;
 }
