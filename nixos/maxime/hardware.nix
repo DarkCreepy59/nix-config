3e9f72ec67983b0cf68520c3acbf819fe3dc8bc3
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 11:18:47 +0100

    Added hardware configuration

--- nixos/maxime/hardware.nix
+++ nixos/maxime/hardware.nix
@@ -1,4 +1,43 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    specialArgs,
+    unstable
 }: {
+    hardware = {
+        opentabletdriver.enable = true;
+        bluetooth.enable = true;
+
+        opengl = {
+            enable = true;
+            driSupport = true;
+            driSupport32Bit = true;
+        };
+
+        nvidia = {
+            modesetting.enable = true;
+
+            powerManagement = {
+                enable = true;
+                finegrained = true;
+            };
+
+            prime = {
+                intelBusId = "PCI:0:2:0";
+                nvidiaBusId = "PCI:1:0:0";
+                offload = {
+                    enable = true;
+                    enableOffloadCmd = true;
+                };
+            };
+        };
+    };
+
+    services = {
+        thermald.enable = true;
+        tlp.enable = true;
+    };
 }
