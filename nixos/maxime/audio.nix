b8d87a514b6a9147d3b08986abebcf0872472a58
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-09 11:23:06 +0100

    Added audio config

--- nixos/maxime/audio.nix
+++ nixos/maxime/audio.nix
@@ -1,4 +1,20 @@
 {
-    ...
+    config,
+    inputs,
+    lib,
+    modulesPath,
+    options,
+    specialArgs,
+    unstable
 }: {
+    services.pipewire = {
+        enable = true;
+
+        alsa.enable = true;
+        pulse.enable = true;
+        jack.enable = true;
+    };
+
+    # Disable pulse because we use pipewire
+    hardware.pulseaudio.enable = false;
 }
