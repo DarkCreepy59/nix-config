e44aee45aececbaeb37d0e89e5b73e493668835b
Maxime Nordier <maxime.nordier@gmail.com>
2024-02-29 13:32:41 +0100

    Add flake inputs to the nix registry

--- nixos/maxime/configuration.nix
+++ nixos/maxime/configuration.nix
@@ -14,11 +14,24 @@
             outputs.overlays.unstable-packages
         ];
     };
+ 
+    nix = {
+        settings = {
+            experimental-features = "nix-command flakes";
+            auto-optimise-store = true;
+        };
 
-    nix.settings = {
-        experimental-features = "nix-command flakes";
-        auto-optimise-store = true;
+        # Map each flake input in the registry
+        registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+        channel.enable = false;
     };
+
+    # And then set the nix path from the content of the registry
+    nix.nixPath = ["/etc/nix/path"];
+    environment.etc = lib.mapAttrs'(name: value: {
+        name = "nix/path/${name}";
+        value.source = value.flake;
+    }) config.nix.registry;
 
     imports = [
         inputs.aagl-on-nix.nixosModules.default
